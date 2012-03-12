2cac36e1bf0eb4443b34d1349720796d8cd1ced7
Daniel Erenrich <daniel@erenrich.net>
2012-03-12 03:13:46 -0700

    fix up style issues + add example mandelbrat file

--- vm.py
+++ vm.py
@@ -4,7 +4,9 @@ from llvm.ee import *
 from parse import AWOT
 
 # How much do we allocate our program
-BUF_SIZE = 4096
+BUF_SIZE = 2048
+
+
 class Compiler:
     def compile(self,prog):
         ast = AWOT().parse(prog)
@@ -17,8 +19,9 @@ class Compiler:
         # allocate our buffer
         int_arr = Type.array(Type.int(),BUF_SIZE)
         self.buf = self.module.add_global_variable(int_arr,'buf')
-        self.buf.section = ".data"
-        self.buf.initializer = self.zero
+        self.buf.section = ".data"        
+        self.buf.initializer = Constant.array(Type.int(), [self.zero] * BUF_SIZE)
+
         # our global pointer
         self.point = self.module.add_global_variable(Type.int(),'point')
         self.point.initializer = self.zero
@@ -28,7 +31,7 @@ class Compiler:
         self.printchar = Function.new(self.module, Type.function(Type.int(), [Type.int()]), 'putchar')
 
         # setup our main function
-        self.main = self.module.add_function(Type.function(Type.int(), [Type.void()]), "main")
+        self.main = self.module.add_function(Type.function(Type.int(),[]), "main")
         bb = self.main.append_basic_block("entry")    
         builder = Builder.new(bb)
         self.build_func(ast,builder)
@@ -40,14 +43,13 @@ class Compiler:
     def build_func(self, ast, builder):
         for c in ast:
             if len(c) > 1:
-                new_func = self.module.add_function(Type.function(Type.void(), [Type.void()]), "func_"+str(self.block_count))
-                self.block_count += 1
-        
+                new_func = self.module.add_function(Type.function(Type.void(), []), "func_"+str(self.block_count))
+                self.block_count += 1        
                 main_block = new_func.append_basic_block("entry")
                 body_block = new_func.append_basic_block("body")
                 end_block = new_func.append_basic_block("end")
                 ret_block = new_func.append_basic_block("ret")
-                builder.call(new_func, (), "tmp")
+                builder.call(new_func, ())
 
                 func_builder = Builder.new(main_block)
                 pv = func_builder.load(self.point)
@@ -98,11 +100,17 @@ class Compiler:
                 builder.call(self.printchar, (v,))
 
 
-hw = "+++++++++++>+>>>>++++++++++++++++++++++++++++++++++++++++++++>++++++++++++++++++++++++++++++++<<<<<<[>[>>>>>>+>+<<<<<<<-]>>>>>>>[<<<<<<<+>>>>>>>-]<[>++++++++++[-<-[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]>[<<[>>>+<<<-]>>[-]]<<]>>>[>>+>+<<<-]>>>[<<<+>>>-]+<[>[-]<[-]]>[<<+>>[-]]<<<<<<<]>>>>>[++++++++++++++++++++++++++++++++++++++++++++++++.[-]]++++++++++<[->-<]>++++++++++++++++++++++++++++++++++++++++++++++++.[-]<<<<<<<<<<<<[>>>+>+<<<<-]>>>>[<<<<+>>>>-]<-[>>.>.<<<[-]]<<[>>+>+<<<-]>>>[<<<+>>>-]<<[<+>-]>[<+>-]<<<-]"
-c = Compiler()
-c.compile(hw)
-#print c.module
-ee = ExecutionEngine.new(c.module)
-retval = ee.run_function(c.main, [])
-print "returned", retval.as_int()
 
+#hw=">++[<+++++++++++++>-]<[[>+>+<<-]>[<+>-]++++++++[>++++++++<-]>.[-]<<>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[>++++++++++[-]<-]<-]<-]<-]<-]<-]<-]++++++++++."
+
+
+
+if __name__ == "__main__":
+    import sys
+    prog = sys.stdin.read()
+    c = Compiler()
+    c.compile(prog)
+    print c.module
+    ee = ExecutionEngine.new(c.module)
+    #retval = ee.run_function(c.main, [])
+    #print "returned", retval.as_int()
